a527224d50b5d1962222aedfcbc5b71cb93e3341
Erick Soares <erick.s.souza@ufv.br>
2024-08-12 07:06:50 -0300

    Verificações do registro

--- app/routes/Register.py
+++ app/routes/Register.py
@@ -1,3 +1,5 @@
+import re
+
 from app import app
 from flask import render_template, request
 
@@ -9,13 +11,31 @@ def cadastro():
     password = request.form.get('password')
     confirm_password = request.form.get('confirm_password')
 
-    if email.strip() == "" or username.strip() == "" or password.strip() == "" or confirm_password.strip() == "":
-        print(render_template('register.html',
-                               required_fields_error="Campos obrigatórios não preenchidos.",
+    if not email.strip() == "" and not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+        return render_template('register.html',
+                               invalid_email_error="Email inválido.",
                                email=email,
                                username=username,
                                password=password,
-                               confirm_password=confirm_password))
+                               confirm_password=confirm_password)
+
+    # if not confirm_password.strip() == "" and verificarSeJaExiste:
+    #     return render_template('register.html',
+    #                            used_email_error="Email digitado já está em uso.",
+    #                            email=email,
+    #                            username=username,
+    #                            password=password,
+    #                            confirm_password=confirm_password)
+
+    if not password.strip() == "" and not re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$', password):
+        return render_template('register.html',
+                               invalid_password_error="Senha fraca. Precisa de tais coisas...",
+                               email=email,
+                               username=username,
+                               password=password,
+                               confirm_password=confirm_password)
+
+    if email.strip() == "" or username.strip() == "" or password.strip() == "" or confirm_password.strip() == "":
         return render_template('register.html',
                                required_fields_error="Campos obrigatórios não preenchidos.",
                                email=email,
