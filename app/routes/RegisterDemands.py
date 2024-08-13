b999e11e4a27ffd10879e173ea38ca318ff10347
juliohfr <jhfrodrigues@gmail.com>
2024-08-13 12:32:34 -0300

    Atualizações no css e validação da data

--- app/routes/RegisterDemands.py
+++ app/routes/RegisterDemands.py
@@ -1,6 +1,26 @@
 from app import app
-from flask import Flask, render_template
+from flask import render_template, request
+from datetime import datetime
 
 @app.route('/cadastrarDemanda', methods=['POST'])
 def cadastrarDemanda():
-    return render_template('registerDemands.html')
+    data = request.form.get("data_show")
+
+    if data == "":
+        return render_template('registerDemands.html',
+                               data_show=data, invalid_date="Campos obrigatórios não preenchidos.")
+
+    try:
+        dataPy = datetime.strptime(data, "%d/%m/%Y")
+
+        if dataPy.date() < datetime.now().date():
+            return render_template('registerDemands.html',
+                           data_show=data, invalid_date="A data informada é anterior ao dia atual.")
+        else:
+            return render_template('registerDemands.html',
+                                   data_show=data)
+    except ValueError:
+        return render_template('registerDemands.html',
+                                   data_show=data, invalid_date="A data informada é inválida.")
+
+
