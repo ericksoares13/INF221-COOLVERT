a1bdceee3abf169449963873b725635fe69a5339
Guilherme Figueiredo <guilhermefigueiredo341@gmail.com>
2024-08-19 11:30:21 -0300

    Fix bug botao voltar chat

--- app/routes/Chat.py
+++ app/routes/Chat.py
@@ -9,16 +9,17 @@ CHAT_HTML = "chat.html"
 @app.route("/chat", methods=["GET", "POST"])
 def chat():
     id_user = session.get("usuárioLogado")["id"]
-    id_outro = session.get('id_outro')["id"]
-    match_id = session.get('match_id')["id"]
-
+    id_outro = session.get('chat')["id_outro"]
+    id_match = session.get('chat')["id_match"]
+    origin_screen = session.get('chat')["origin_screen"]
+    
     minha_imagem = BancoDeDados.GetImagemPerfil(id_user)
     outra_imagem = BancoDeDados.GetImagemPerfil(id_outro)
     
     user_obj = BancoDeDados.GetUser(id_user)
     outro_obj = BancoDeDados.GetUser(id_outro)
     
-    chat = BancoDeDados.GetChat(match_id)
+    chat = BancoDeDados.GetChat(id_match)
     mensagens = []
     for mensagem in chat:
         mensagens.append({
@@ -27,7 +28,7 @@ def chat():
             'imagem_perfil': minha_imagem.caminho if (mensagem.dono == id_user) else outra_imagem.caminho,
         })
 
-    return render_template(CHAT_HTML, mensagens=mensagens, user=user_obj, other_user=outro_obj)
+    return render_template(CHAT_HTML, mensagens=mensagens, user=user_obj, other_user=outro_obj, origin_screen=origin_screen)
 
 
 @app.route("/send_message", methods=["POST"])
@@ -47,6 +48,9 @@ def send_message():
 
 @app.route("/chatDetails", methods=["GET", "POST"])
 def chat_details():
-    session["id_outro"] = {"id": request.form.get('chat_id_outro')}
-    session["match_id"] = {"id": request.form.get('chat_id_match')}
+    session["chat"] = {
+        "id_outro": request.form.get('chat_id_outro'),
+        "id_match": request.form.get('chat_id_match'),
+        "origin_screen": request.form.get('origin_screen')
+    }
     return redirect(url_for("chat"))
